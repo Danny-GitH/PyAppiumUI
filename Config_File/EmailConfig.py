c4b32b128e7a63a2af31eeaff26eabd727c27f4d
Danny-GitH <30306254+Danny-GitH@users.noreply.github.com>
2020-11-30 18:33:15 +0800

    Update EmailConfig.py

--- Config_File/EmailConfig.py
+++ Config_File/EmailConfig.py
@@ -4,6 +4,6 @@ __author__ = 'dingrui'
 # 测试邮箱配置
 Smtp_Server = 'smtp.163.com'
 Smtp_Sender = '15656550098@163.com'
-Smtp_Sender_Password = "Wangyuzhu520"
+Smtp_Sender_Password = "@@@@@@@"
 #Smtp_Receiver = ['yuzhu.wang@mathartsys.com']
 Smtp_Receiver = ['15656550098@163.com']
